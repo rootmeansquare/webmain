beffcd57efffa1e021aba64eedbe1f66afd66489
T03690 <T03690@v1020-wn-120-41.campus-dynamic.uwaterloo.ca>
2014-12-13 21:58:15 -0500

    added bootstrap, created server, fiddled with css

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use('/',express.static('../'));
+
+app.use('/resume',express.static('../resume.html'));
+app.use('/resumepdf',express.static('../resume.pdf'));
+
+var server = app.listen(3000,function(){
+	console.log('Listening on port 3000')
+});
