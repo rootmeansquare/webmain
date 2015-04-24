94d6b348588bcac9fd171f95edffa9d6cf5c02e7
Imaad Umar <imaadumar@gmail.com>
2015-04-24 01:21:14 -0400

    files location changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use('/',express.static(__dirname+".."));
+app.use('/',express.static(__dirname+"../frontend"));
 
-app.use('/resume',express.static('../resume.html'));
-app.use('/resumepdf',express.static('../resume.pdf'));
+app.use('/resume',express.static('../frontend/resume.html'));
+app.use('/resumepdf',express.static('../auxFiles/resume.pdf'));
 
 var server = app.listen(3000,function(){
 	console.log('Listening on port 3000')
