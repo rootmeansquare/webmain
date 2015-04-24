db54d6a6ccfaab07bef0cb72d36918b97fa38257
Imaad Umar <imaadumar@gmail.com>
2015-04-24 01:24:19 -0400

    fixing picture url and dirname

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use('/',express.static(__dirname+"../frontend"));
 
 app.use('/resume',express.static('../frontend/resume.html'));
-app.use('/resumepdf',express.static('../auxFiles/resume.pdf'));
+app.use('/resumepdf',express.static(__dirname+'../auxFiles/resume.pdf'));
 
 var server = app.listen(3000,function(){
 	console.log('Listening on port 3000')
